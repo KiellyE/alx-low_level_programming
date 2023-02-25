4acff70f21a08b394aa197cac01585aec89cdfb1
KiellyE <ezekielimereti@gmail.com>
2023-02-25 06:10:31 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: 'get the last digit of a number'
+ * Description: 'Check for number if it is positive or negative'
  * Return: always 0
  */
 int main(void)
