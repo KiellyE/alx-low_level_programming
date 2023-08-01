373f669a72377bd989d68bf1e45004ab8759d53b
KiellyE <ezekielimereti@gmail.com>
2023-07-31 22:00:01 -0700

    even more

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strspn - entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (success)
+ */
+
+unsigned int _strspn(char *s, char *accept);
+{
+	unsigned int n = 0;
+	int r;
+
+	while (*s)
+	{
+		for (r = 0; accept[r]; r++)
+		{
+			if unsigned int _strspn(char *s, char *accept);
+		}
+		s++;
+	}
+	return (n);
+}
